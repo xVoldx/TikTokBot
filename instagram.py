5150064fdc70ae9662e9dc751d8735d7837a82c2
Py is Py <62064902+xVoldx@users.noreply.github.com>
2021-07-24 16:34:37 +0100

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -26,7 +26,6 @@ class Parser:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.binary_location = chrome_bin        
         # add the mobile emulation to the chrome options variable
         chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 
